e96777a35a27c4595852582743b9b962c22ab4e8
Minaya Usuki <usuki@aizulab.com>
2022-02-18 01:09:09 +0000

    開発環境自動構築

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -1,23 +1,19 @@
 #!/bin/bash -eu
 
+# Dockerfile.devの実行後に実行される
+# bind後の処理を行う
+# 設定は.devcontainer/devcontainer.jsonに記載
+
 cd /opt/COIAS_program_github
 conda env create -f env.yml
 
-export CONDA_DEFAULT_ENV=COIAS_program_github
+conda activate COIAS_program_github
 
 # condaのCOIAS_program_github環境下で、ビルド
-chmod -R 700  ./*
+chmod -R 700 /opt/COIAS_program_github
 /root/miniconda3/envs/COIAS_program_github/bin/python setup12.py build_ext --inplace
 
 # Cythonのビルド
 cd /opt/COIAS_program_github/findOrb
 make -f linlunar.mak
 make -f linmake
-
-exec $SHELL -l
-
-# APIを作業ディレクトリとする
-cd /opt/COIAS_program_github/API
-
-# API server を開始
-/root/miniconda3/envs/COIAS_program_github/bin/uvicorn main:app --host 0.0.0.0 --reload
