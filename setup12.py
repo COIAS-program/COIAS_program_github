448e69df08c3d63b0005935ea1b07e2a1747dcca
Minaya Usuki <usuki@aizulab.com>
2022-02-28 01:08:14 +0000

    ファイル整形

--- setup12.py
+++ setup12.py
@@ -6,11 +6,10 @@ from distutils.extension import Extension
 import numpy
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("mktraclet", ["mktraclet.pyx"], include_dirs=[numpy.get_include()])]
+ext_modules = [
+    Extension("mktraclet", ["mktraclet.pyx"], include_dirs=[numpy.get_include()])
+]
 
-setup(
-    cmdclass={'build_ext': build_ext},
-    ext_modules=ext_modules
-)
+setup(cmdclass={"build_ext": build_ext}, ext_modules=ext_modules)
 
 # python setup12.py build_ext --inplace
