14f66470101bad90beea6075ca4d0a8146f24fbe
usuki <usuki@aizualb.com>
2022-03-30 16:06:04 +0900

    conda activateもれ

--- script/startup.sh
+++ script/startup.sh
@@ -1,4 +1,8 @@
-#!/bin/bash -eu
+#!/bin/bash -su
+
+# coiasをデフォルトに設定
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate coias
 
 git pull
 /root/miniconda3/envs/coias/bin/uvicorn API.main:app --host 0.0.0.0 --reload
