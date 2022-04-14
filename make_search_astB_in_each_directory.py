a9404453c7e1f7e40422390683ed2d7049bf1cc7
COIAS-program <sugiuraks@elsi.jp>
2022-04-14 16:54:16 +0900

    一度精密軌道問い合わせをしたものは情報を保持しておいて二度目は問合せしないように変更
    fitsファイルのra, dec, jdごとにJPLに問い合わせをした既知天体の精密軌道を~/.coias/orbit_data/連番ディレクトリ/以下に保持しておき, 次に同じra, dec, jdのfitsファイルを解析するときは精密軌道を問合せせずに~/.coias/orbit_data/連番ディレクトリ/に保持してあるデータを使用するように変更
    ただし, 精密軌道データが2週間以上古い場合は同じra, dec, jdの精密軌道データを持っていても更新する
    実行時間については, 5枚のfitsファイル全てに対し2週間以内に同じra, dec, jdの精密軌道を問い合わせしていればprempsearchCの内容を完全に飛ばすが, 5枚のうち1枚でも2週間以内に問い合わせをしていないものがある場合は今までと実行時間は変わらない

---
+++ make_search_astB_in_each_directory.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*
+import os
+import sys
+import subprocess
+
+#---read precise_orbit_directories.txt---------------------------------------
+Ndata = 5
+directoryNames = []
+isCorrectDirectory = []
+preciseOrbitDirectoriesFile = open("precise_orbit_directories.txt","r")
+for i in range(Ndata):
+    content = preciseOrbitDirectoriesFile.readline().split()
+    directoryNames.append(content[0])
+    isCorrectDirectory.append(int(content[1]))
+preciseOrbitDirectoriesFile.close()
+#----------------------------------------------------------------------------
+
+#---process numbered_new2B.txt and karifugo_new2B.txt in each directory------
+for i in range(Ndata):
+    if isCorrectDirectory[i]==0:
+        subprocess.run("grep -E \"^19|^20\" {0}/karifugo_new2B.txt > {0}/karifugo_new2C.txt".format(directoryNames[i]),shell=True)
+        subprocess.run("cat {0}/numbered_new2B.txt {0}/karifugo_new2C.txt |sort -n > {0}/search_astpre.txt".format(directoryNames[i]),shell=True)
+        subprocess.run("sed 's/--/30.0/g;' {0}/search_astpre.txt |sed 's/ (1999 19 //g;' |sed 's/ (1981 2 //g;'|sed 's/ - / /g;'> {0}/search_astB.txt".format(directoryNames[i]),shell=True)
+#----------------------------------------------------------------------------
