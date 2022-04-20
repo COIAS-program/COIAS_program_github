f9005f31998c597a6f53125ef30aa3bb2ea736b4
COIAS-program <sugiuraks@elsi.jp>
2022-04-20 14:23:04 +0900

    明るい既知天体をレポートから除外するスクリプトの追加
    AstMPC.edbに記載の15等級よりも明るくなり得る既知天体をレポートから除外するスクリプトを追加した.

---
+++ reject_bright_known_asteroids_from_report.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*
+
+#---get names of bright known asteroids-----------------
+fileBrightKnownAsteroids = open("bright_asteroid_MPC_names_in_the_field.txt","r")
+lines = fileBrightKnownAsteroids.readlines()
+namesOfBrightKnownAsteroids = []
+for l in range(len(lines)):
+    namesOfBrightKnownAsteroids.append(lines[l].rstrip("\n"))
+fileBrightKnownAsteroids.close()
+#-------------------------------------------------------
+
+#---get data in send_mpc.txt----------------------------
+inputfileSendMpc = open("send_mpc.txt","r")
+lines = inputfileSendMpc.readlines()
+inputfileSendMpc.close()
+#-------------------------------------------------------
+
+#---compare names in send_mpc.txt and reject bright known asteroids---
+outputfileSendMpc = open("send_mpc.txt","w",newline="\n")
+for l in range(len(lines)):
+    oneLineList = lines[l].split()
+    flag = 0
+    for l2 in range(len(namesOfBrightKnownAsteroids)):
+        if oneLineList[0] == namesOfBrightKnownAsteroids[l2]:
+            flag = 1
+    if flag == 0:
+        outputfileSendMpc.write(lines[l])
+outputfileSendMpc.close()
+#---------------------------------------------------------------------
