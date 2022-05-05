e8c60f6f0e906c22870a9571e84e251559f10599
COIAS-program <sugiuraks@elsi.jp>
2022-05-06 08:15:42 +0900

    GUI COIASとのミラーリング5

--- src6_between_COIAS_and_ReCOIAS/prempedit3.py
+++ src6_between_COIAS_and_ReCOIAS/prempedit3.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*
-import os
 import sys
-import itertools
 import re
-import subprocess
 
 logFile = open("log.txt","a",newline="\n")
 print("prempedit3.py begins")
 logFile.write("prempedit3.py begins\n")
 
-#cmd = 'cut -b 6-12 mpc2.txt | grep ^H | uniq > hoge.txt'
-#cmd = 'cut -b 6-12 mpc2.txt' 
-#subprocess.call(cmd.split())
-#path_name = os.getcwd()
-#detect list
+# cmd = 'cut -b 6-12 mpc2.txt | grep ^H | uniq > hoge.txt'
+# cmd = 'cut -b 6-12 mpc2.txt' 
+
+# detect list
 tmp1 = "Hlist.txt"
 tmp2 = "mpc2.txt"
 tmp3 = "mpc3.txt"
@@ -24,20 +20,20 @@ data2 = open(tmp2,"r")
 lines = data1.readlines()
 lines2 = data2.readlines()
 
-#input first number
+# input first number
 args = sys.argv
 if len(args) <= 1:
     print("Please input the first H number.")
     logFile.write("Please input the first H number.\n")
 else:
-#args= [0,0,0]
+    # args= [0,0,0]
 
-#make befor Hlist
+    # make befor Hlist
     Hlist1 = []
     for i in lines:
         Hlist1.append(i.rstrip('\n'))
 
-#make after Hlist
+    # make after Hlist
     Hlist2 = []
     for k in range(len(lines)):
         k = k + int(args[1])
@@ -51,30 +47,30 @@ else:
                 tmp = lines2[n].replace(Hlist1[m],Hlist2[m])
                 Hlist3.append(tmp)
     
-#        tmp = lines2[n].replace(Hlist1[m],Hlist2[m])
-#    Hlist3.append()
+                # tmp = lines2[n].replace(Hlist1[m],Hlist2[m])
+                # Hlist3.append()
 
-#output H conversion list 2022.4.27 K.S.
+    # output H conversion list 2022.4.27 K.S.
     fileHConversionList = open("H_conversion_list.txt","w",newline="\n")
     for l in range(len(Hlist1)):
         fileHConversionList.write(Hlist1[l] + ' ' + Hlist2[l] + '\n')
     fileHConversionList.close()
 
 
-#kokokara Jan.22.2020
+    # kokokara Jan.22.2020
     new_list3 = []
     for l in range(len(lines2)): 
         if re.match('\w',lines2[l]):
-#        print(lines[l])
+            # print(lines[l])
             new_list3.append(lines2[l])
         elif re.match('~',lines2[l]):
-#        print(lines[l])
+            # print(lines[l])
             new_list3.append(lines2[l])
         elif re.match('^     K',lines2[l]):
-#        print(lines[l])
+            # print(lines[l])
             new_list3.append(lines2[l])
         elif re.match('^     J',lines2[l]):
-            #        print(lines[l])
+            # print(lines[l])
             new_list3.append(lines2[l])
         
     new_list4 = new_list3 + Hlist3  
