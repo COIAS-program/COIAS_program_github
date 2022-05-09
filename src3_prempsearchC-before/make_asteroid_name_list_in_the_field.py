7415ce3cdd84c8965953eac6d750231f21b1253a
COIAS-program <sugiuraks@elsi.jp>
2022-05-09 13:56:39 +0900

    エラーハンドリング: src3

--- src3_prempsearchC-before/make_asteroid_name_list_in_the_field.py
+++ src3_prempsearchC-before/make_asteroid_name_list_in_the_field.py
@@ -3,88 +3,128 @@
 import subprocess
 import re
 import numpy as np
+import traceback
 from changempc import *
 
 
-#---read precise_orbit_directories.txt------------------------------------------------------
-Ndata = 5
-directoryNames = []
-isCorrectDirectory = []
-preciseOrbitDirectoriesFile = open("precise_orbit_directories.txt","r")
-for i in range(Ndata):
-    content = preciseOrbitDirectoriesFile.readline().split()
-    directoryNames.append(content[0])
-    isCorrectDirectory.append(int(content[1]))
-preciseOrbitDirectoriesFile.close()
-#-------------------------------------------------------------------------------------------
+try:
+    #---read precise_orbit_directories.txt------------------------------------------------------
+    Ndata = 5
+    directoryNames = []
+    isCorrectDirectory = []
+    preciseOrbitDirectoriesFile = open("precise_orbit_directories.txt","r")
+    for i in range(Ndata):
+        content = preciseOrbitDirectoriesFile.readline().split()
+        directoryNames.append(content[0])
+        isCorrectDirectory.append(int(content[1]))
+    preciseOrbitDirectoriesFile.close()
+    #-------------------------------------------------------------------------------------------
 
-#---make bright asteroid MPC name list in each directory------------------------------------
-## remove a character "\"
-subprocess.run(r"sed -e 's/\\//g' bright_asteroid_raw_names_in_the_field.txt > bright_asteroid_raw_names_in_the_field2.txt",shell=True)
-## subtract number from numbered asteroids and karifugo from karifugo asteroids
-subprocess.run("awk -F' ' '$1 ~ /^[1-9]/ && $2 ~ /^[A-Z]/ && $2 !~ /[A-Z][A-Z][0-9]*/ && $2 != 'P-L' && $2 != 'T-1' && $2 != 'T-2' && $2 != 'T-3' {print $1}' bright_asteroid_raw_names_in_the_field2.txt > bright_asteroid_raw_names_in_the_field3.txt",shell=True)
-subprocess.run("awk -F' ' '$1 ~ /^[1-9]/ && $2 ~ /^[1-2]/ || $3 == 'P-L' || $3 == 'T-1' || $3 == 'T-2' || $3 == 'T-3' {print $1}' bright_asteroid_raw_names_in_the_field2.txt >> bright_asteroid_raw_names_in_the_field3.txt",shell=True)
-subprocess.run("awk -F' ' '$1 ~ /^[1-2]/ && $2 ~ /[A-Z][A-Z][0-9]*/ || $2 == 'P-L' || $2 == 'T-1' || $2 == 'T-2' || $2 == 'T-3' {print $1,$2}' bright_asteroid_raw_names_in_the_field2.txt >> bright_asteroid_raw_names_in_the_field3.txt",shell=True)
-## remove space between karifugo
-subprocess.run("sed 's/ //g' bright_asteroid_raw_names_in_the_field3.txt > bright_asteroid_raw_names_in_the_field4.txt",shell=True)
+    #---make bright asteroid MPC name list in each directory------------------------------------
+    errorList=[]
+    ## remove a character "\"
+    completed_process = subprocess.run(r"sed -e 's/\\//g' bright_asteroid_raw_names_in_the_field.txt > bright_asteroid_raw_names_in_the_field2.txt",shell=True)
+    errorList.append(completed_process.returncode)
+    ## subtract number from numbered asteroids and karifugo from karifugo asteroids
+    completed_process = subprocess.run("awk -F' ' '$1 ~ /^[1-9]/ && $2 ~ /^[A-Z]/ && $2 !~ /[A-Z][A-Z][0-9]*/ && $2 != 'P-L' && $2 != 'T-1' && $2 != 'T-2' && $2 != 'T-3' {print $1}' bright_asteroid_raw_names_in_the_field2.txt > bright_asteroid_raw_names_in_the_field3.txt",shell=True)
+    errorList.append(completed_process.returncode)
+    completed_process = subprocess.run("awk -F' ' '$1 ~ /^[1-9]/ && $2 ~ /^[1-2]/ || $3 == 'P-L' || $3 == 'T-1' || $3 == 'T-2' || $3 == 'T-3' {print $1}' bright_asteroid_raw_names_in_the_field2.txt >> bright_asteroid_raw_names_in_the_field3.txt",shell=True)
+    errorList.append(completed_process.returncode)
+    completed_process = subprocess.run("awk -F' ' '$1 ~ /^[1-2]/ && $2 ~ /[A-Z][A-Z][0-9]*/ || $2 == 'P-L' || $2 == 'T-1' || $2 == 'T-2' || $2 == 'T-3' {print $1,$2}' bright_asteroid_raw_names_in_the_field2.txt >> bright_asteroid_raw_names_in_the_field3.txt",shell=True)
+    errorList.append(completed_process.returncode)
+    ## remove space between karifugo
+    completed_process = subprocess.run("sed 's/ //g' bright_asteroid_raw_names_in_the_field3.txt > bright_asteroid_raw_names_in_the_field4.txt",shell=True)
+    errorList.append(completed_process.returncode)
 
-## transform to MPC format name
-inputFile = open("bright_asteroid_raw_names_in_the_field4.txt","r")
-lines = inputFile.readlines()
-inputFile.close()
-brightAsteroidsMPCNames = []
-for l in range(len(lines)):
-    if re.search(r'[a-zA-Z]',lines[l]):
-        brightAsteroidsMPCNames.append(get_MPC_format_name_for_karifugo_asteroids(lines[l].rstrip("\n")))
-    else:
-        brightAsteroidsMPCNames.append(get_MPC_format_name_for_numbered_asteroids(lines[l].rstrip("\n")))
+    for e in errorList:
+        if e!=0:
+            raise FileNotFoundError
 
-## output to each directory
-for n in range(Ndata):
-    outputFile = open(directoryNames[n]+"/bright_asteroid_MPC_names_in_the_field.txt","w",newline="\n")
-    for l in range(len(brightAsteroidsMPCNames)):
-        outputFile.write(brightAsteroidsMPCNames[l]+"\n")
-    outputFile.close()
-#-------------------------------------------------------------------------------------------
+    ## transform to MPC format name
+    inputFile = open("bright_asteroid_raw_names_in_the_field4.txt","r")
+    lines = inputFile.readlines()
+    inputFile.close()
+    brightAsteroidsMPCNames = []
+    for l in range(len(lines)):
+        if re.search(r'[a-zA-Z]',lines[l]):
+            brightAsteroidsMPCNames.append(get_MPC_format_name_for_karifugo_asteroids(lines[l].rstrip("\n")))
+        else:
+            brightAsteroidsMPCNames.append(get_MPC_format_name_for_numbered_asteroids(lines[l].rstrip("\n")))
 
-#---make name conversion list of all known asteroids in the field in each directory---------
-## subtract number and name from numbered and karifugo asteroids
-subprocess.run("awk -F' ' '$2 ~ /^[1-9]/ && $3 ~ /^[A-Z]/ && $3 !~ /[A-Z][A-Z][0-9]*/ && $3 != 'P-L' && $3 != 'T-1' && $3 != 'T-2' && $3 != 'T-3' {print $2,$3}' cand2b.txt > candNameList_numbered_named.txt",shell=True)
-subprocess.run("awk -F' ' '$2 ~ /^[1-9]/ && $3 ~ /^[1-2]/ || $4 == 'P-L' || $4 == 'T-1' || $4 == 'T-2' || $4 == 'T-3' {print $2,$3,$4}' cand2b.txt > candNameList_numbered_unnamed.txt",shell=True)
-subprocess.run("awk -F' ' '$2 ~ /^[1-2]/ && $3 ~ /[A-Z][A-Z][0-9]*/ || $3 == 'P-L' || $3 == 'T-1' || $3 == 'T-2' || $3 == 'T-3' {print $2,$3}' cand2b.txt > candNameList_karifugo.txt",shell=True)
+    ## output to each directory
+    for n in range(Ndata):
+        outputFile = open(directoryNames[n]+"/bright_asteroid_MPC_names_in_the_field.txt","w",newline="\n")
+        for l in range(len(brightAsteroidsMPCNames)):
+            outputFile.write(brightAsteroidsMPCNames[l]+"\n")
+        outputFile.close()
+    #-------------------------------------------------------------------------------------------
 
-## for numbered and named asteroids
-numberedNamedFullName = np.loadtxt("candNameList_numbered_named.txt",dtype='str')
-numberedNamedShortName = []
-numberedNamedMPCName = []
-for l in range(len(numberedNamedFullName)):
-    numberedNamedShortName.append(numberedNamedFullName[l][0])
-    numberedNamedMPCName.append(get_MPC_format_name_for_numbered_asteroids(numberedNamedFullName[l][0]))
+    #---make name conversion list of all known asteroids in the field in each directory---------
+    ## subtract number and name from numbered and karifugo asteroids
+    completed_process = subprocess.run("awk -F' ' '$2 ~ /^[1-9]/ && $3 ~ /^[A-Z]/ && $3 !~ /[A-Z][A-Z][0-9]*/ && $3 != 'P-L' && $3 != 'T-1' && $3 != 'T-2' && $3 != 'T-3' {print $2,$3}' cand2b.txt > candNameList_numbered_named.txt",shell=True)
+    errorList.append(completed_process.returncode)
+    completed_process = subprocess.run("awk -F' ' '$2 ~ /^[1-9]/ && $3 ~ /^[1-2]/ || $4 == 'P-L' || $4 == 'T-1' || $4 == 'T-2' || $4 == 'T-3' {print $2,$3,$4}' cand2b.txt > candNameList_numbered_unnamed.txt",shell=True)
+    errorList.append(completed_process.returncode)
+    completed_process = subprocess.run("awk -F' ' '$2 ~ /^[1-2]/ && $3 ~ /[A-Z][A-Z][0-9]*/ || $3 == 'P-L' || $3 == 'T-1' || $3 == 'T-2' || $3 == 'T-3' {print $2,$3}' cand2b.txt > candNameList_karifugo.txt",shell=True)
+    errorList.append(completed_process.returncode)
 
-## for numbered and unnamed asteroids
-numberedUnnamedFullName = np.loadtxt("candNameList_numbered_unnamed.txt",dtype='str')
-numberedUnnamedShortName = []
-numberedUnnamedMPCName = []
-for l in range(len(numberedUnnamedFullName)):
-    numberedUnnamedShortName.append(numberedUnnamedFullName[l][0])
-    numberedUnnamedMPCName.append(get_MPC_format_name_for_numbered_asteroids(numberedUnnamedFullName[l][0]))
+    for e in errorList:
+        if e!=0:
+            raise FileNotFoundError
 
-## for karifugo asteroids
-karifugoFullName = np.loadtxt("candNameList_karifugo.txt",dtype='str')
-karifugoShortName = []
-karifugoMPCName = []
-for l in range(len(karifugoFullName)):
-    karifugoShortName.append(karifugoFullName[l][0]+karifugoFullName[l][1])
-    karifugoMPCName.append(get_MPC_format_name_for_karifugo_asteroids(karifugoShortName[l]))
-
-## output to each directory
-for n in range(Ndata):
-    outputFile = open(directoryNames[n]+"/name_conversion_list_in_the_field.txt","w",newline="\n")
+    ## for numbered and named asteroids
+    numberedNamedFullName = np.loadtxt("candNameList_numbered_named.txt",dtype='str')
+    numberedNamedShortName = []
+    numberedNamedMPCName = []
     for l in range(len(numberedNamedFullName)):
-        outputFile.write(numberedNamedFullName[l][0]+" "+numberedNamedFullName[l][1]+","+numberedNamedShortName[l]+","+numberedNamedMPCName[l]+"\n")
+        numberedNamedShortName.append(numberedNamedFullName[l][0])
+        numberedNamedMPCName.append(get_MPC_format_name_for_numbered_asteroids(numberedNamedFullName[l][0]))
+
+    ## for numbered and unnamed asteroids
+    numberedUnnamedFullName = np.loadtxt("candNameList_numbered_unnamed.txt",dtype='str')
+    numberedUnnamedShortName = []
+    numberedUnnamedMPCName = []
     for l in range(len(numberedUnnamedFullName)):
-        outputFile.write(numberedUnnamedFullName[l][0]+" "+numberedUnnamedFullName[l][1]+" "+numberedUnnamedFullName[l][2]+","+numberedUnnamedShortName[l]+","+numberedUnnamedMPCName[l]+"\n")
+        numberedUnnamedShortName.append(numberedUnnamedFullName[l][0])
+        numberedUnnamedMPCName.append(get_MPC_format_name_for_numbered_asteroids(numberedUnnamedFullName[l][0]))
+
+    ## for karifugo asteroids
+    karifugoFullName = np.loadtxt("candNameList_karifugo.txt",dtype='str')
+    karifugoShortName = []
+    karifugoMPCName = []
     for l in range(len(karifugoFullName)):
-        outputFile.write(karifugoFullName[l][0]+" "+karifugoFullName[l][1]+","+karifugoShortName[l]+","+karifugoMPCName[l]+"\n")
-    outputFile.close()
-#-------------------------------------------------------------------------------------------
+        karifugoShortName.append(karifugoFullName[l][0]+karifugoFullName[l][1])
+        karifugoMPCName.append(get_MPC_format_name_for_karifugo_asteroids(karifugoShortName[l]))
+
+    ## output to each directory
+    for n in range(Ndata):
+        outputFile = open(directoryNames[n]+"/name_conversion_list_in_the_field.txt","w",newline="\n")
+        for l in range(len(numberedNamedFullName)):
+            outputFile.write(numberedNamedFullName[l][0]+" "+numberedNamedFullName[l][1]+","+numberedNamedShortName[l]+","+numberedNamedMPCName[l]+"\n")
+        for l in range(len(numberedUnnamedFullName)):
+            outputFile.write(numberedUnnamedFullName[l][0]+" "+numberedUnnamedFullName[l][1]+" "+numberedUnnamedFullName[l][2]+","+numberedUnnamedShortName[l]+","+numberedUnnamedMPCName[l]+"\n")
+        for l in range(len(karifugoFullName)):
+            outputFile.write(karifugoFullName[l][0]+" "+karifugoFullName[l][1]+","+karifugoShortName[l]+","+karifugoMPCName[l]+"\n")
+        outputFile.close()
+    #-------------------------------------------------------------------------------------------
+
+except FileNotFoundError:
+    print("Some previous files are not found in make_asteroid_name_list_in_the_field.py!")
+    print(traceback.format_exc())
+    error = 1
+    errorReason = 34
+
+except Exception:
+    print("Some errors occur in make_asteroid_name_list_in_the_field.py!")
+    print(traceback.format_exc())
+    error = 1
+    errorReason = 35
+
+else:
+    error = 0
+    errorReason = 34
+
+finally:
+    errorFile = open("error.txt","a")
+    errorFile.write("{0:d} {1:d} 313 \n".format(error,errorReason))
+    errorFile.close()
--- src3_prempsearchC-before/searchB.py
+++ src3_prempsearchC-before/searchB.py
@@ -6,54 +6,13 @@ from astropy.io import fits
 import ephem
 import julian
 import time
+import traceback
 
 # multiprocessing
 from multiprocessing import Pool
 
-# ast list
-tmp1 = str("hoge")
-data1 = np.loadtxt(tmp1,delimiter = ',',dtype='str')
-
-# read scidata
-scidata1 = fits.open('warp1_bin.fits')
-scidata2 = fits.open('warp2_bin.fits')
-scidata3 = fits.open('warp3_bin.fits')
-scidata4 = fits.open('warp4_bin.fits')
-scidata5 = fits.open('warp5_bin.fits')
-
-jd1 = scidata1[0].header['JD']
-jd2 = scidata2[0].header['JD']
-jd3 = scidata3[0].header['JD']
-jd4 = scidata4[0].header['JD']
-jd5 = scidata5[0].header['JD']
-
-
-# ra dec
-ra = scidata1[0].header['CRVAL1']
-dec = scidata1[0].header['CRVAL2']
-# search region
-# search region 2 =>1.5 2020.5.29
-ra_min = ra-1.8
-ra_max = ra+1.8
-dec_min =  dec-1.8
-dec_max =  dec+1.8
-
-# site information
-subaru = ephem.Observer()
-subaru.lon =  '-155.4767'
-subaru.lat = '19.8256'
-
-subaru.elevation = 4139
-dt = julian.from_jd(float(jd1),fmt='jd')
-d = ephem.Date(dt)
-subaru.date = d
-
-t1 = time.time()
-
-
-
+#---function-------------------------------------------------------------------
 # mp.ra value is radian
-
 def search(i):
     tmp4 = []
     tmp3 = str(data1[i]).replace("\' \'",",").replace("\'\n \'",",").replace(" ","\ '").replace("[","").replace("]","").replace("\'","") 
@@ -72,14 +31,81 @@ def search(i):
         # return tmp5
         # print(tmp4)
     return tmp4
+#------------------------------------------------------------------------------
 
-if __name__ == "__main__":
-    with Pool(31) as p:
-        tmp10 = p.map(search,range(len(data1)))
-        tmp11 = [l for l in tmp10 if l]
-        tmp12 = np.array(tmp11)
-        tmp12 = tmp12.reshape(len(tmp12),5)  
-        np.savetxt('cand_dim.txt',tmp12,fmt='%s')
-        t2 = time.time()
-        elapsed_time = t2 -t1
-        # print(elapsed_time)
+try:
+    # ast list
+    tmp1 = str("hoge")
+    data1 = np.loadtxt(tmp1,delimiter = ',',dtype='str')
+
+    # read scidata
+    scidata1 = fits.open('warp1_bin.fits')
+    scidata2 = fits.open('warp2_bin.fits')
+    scidata3 = fits.open('warp3_bin.fits')
+    scidata4 = fits.open('warp4_bin.fits')
+    scidata5 = fits.open('warp5_bin.fits')
+
+    jd1 = scidata1[0].header['JD']
+    jd2 = scidata2[0].header['JD']
+    jd3 = scidata3[0].header['JD']
+    jd4 = scidata4[0].header['JD']
+    jd5 = scidata5[0].header['JD']
+
+
+    # ra dec
+    ra = scidata1[0].header['CRVAL1']
+    dec = scidata1[0].header['CRVAL2']
+    # search region
+    # search region 2 =>1.5 2020.5.29
+    ra_min = ra-1.8
+    ra_max = ra+1.8
+    dec_min =  dec-1.8
+    dec_max =  dec+1.8
+
+    # site information
+    subaru = ephem.Observer()
+    subaru.lon =  '-155.4767'
+    subaru.lat = '19.8256'
+
+    subaru.elevation = 4139
+    dt = julian.from_jd(float(jd1),fmt='jd')
+    d = ephem.Date(dt)
+    subaru.date = d
+
+    t1 = time.time()
+
+    if __name__ == "__main__":
+        with Pool(31) as p:
+            tmp10 = p.map(search,range(len(data1)))
+            tmp11 = [l for l in tmp10 if l]
+            tmp12 = np.array(tmp11)
+            tmp12 = tmp12.reshape(len(tmp12),5)  
+            np.savetxt('cand_dim.txt',tmp12,fmt='%s')
+            t2 = time.time()
+            elapsed_time = t2 -t1
+            # print(elapsed_time)
+
+except FileNotFoundError:
+    if __name__ == "__main__":
+        print("Some previous files are not found in searchB.py!")
+        print(traceback.format_exc())
+        error = 1
+        errorReason = 34
+
+except Exception:
+    if __name__ == "__main__":
+        print("Some errors occur in searchB.py!")
+        print(traceback.format_exc())
+        error = 1
+        errorReason = 35
+
+else:
+    if __name__ == "__main__":
+        error = 0
+        errorReason = 34
+
+finally:
+    if __name__ == "__main__":
+        errorFile = open("error.txt","a")
+        errorFile.write("{0:d} {1:d} 305 \n".format(error,errorReason))
+        errorFile.close()
