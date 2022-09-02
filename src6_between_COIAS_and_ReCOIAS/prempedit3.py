223ed7d60a219253d5869a7fb24fd18525c3db0a
COIAS-program <sugiuraks@elsi.jp>
2022-09-02 13:19:09 +0900

    AstsearchR_between_COIAS_and_ReCOIASの実行時に第二引数を指定しなかった場合には~/.coias/param/max_H_number.txtに記載の番号からの連番になるようにした.

--- src6_between_COIAS_and_ReCOIAS/prempedit3.py
+++ src6_between_COIAS_and_ReCOIAS/prempedit3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*
-#Timestamp: 2022/08/05 20:00 sugiura
+#Timestamp: 2022/09/02 13:00 sugiura
 #############################################################################################
 # このスクリプトの実行時には第二引数に自分が今まで見つけてきた新天体のH番号の最大値+1を指定する.
 # (AstsearchR_between_COIAS_and_ReCOIAS実行時の第二引数が自動的にこのスクリプトの第二引数に指定される)
+# 引数なしでこのスクリプトが実行された場合には, ~/.coias/param/max_H_number.txtに記載の番号が使用される.
 # Hlist.txtに記載のH番号を第二引数から始まる連番になるようにH番号の変換のリストを作成し,
 # mpc2.txtに記載の新天体の名前をその変換の通りに変えたものをmpc3.txtに出力する.
 # つまり, mpc2.txtに記載の新天体の名前を第二引数から始まる連番になるように付け替える.
@@ -17,6 +18,7 @@
 # 　　    書式: 変換前の新天体の名前 変換後に連番となった新天体の名前
 #############################################################################################
 import sys
+import os
 import re
 import traceback
 
@@ -38,63 +40,69 @@ try:
 
     # input first number
     args = sys.argv
-    if len(args) <= 1:
-        print("####  PLEASE INPUT THE FIRST H NUMBER!!! ####")
-        logFile.write("####  PLEASE INPUT THE FIRST H NUMBER!!! ####\n")
-        raise ValueError
+    if len(args) == 1:
+        maxHFileName = os.path.expanduser("~") + "/.coias/param/max_H_number.txt"
+        f = open(maxHFileName, "r")
+        line = f.readline()
+        f.close()
+
+        firstH = int(line.rstrip("\n"))
+    elif len(args) == 2:
+        firstH = int(args[1])
     else:
-        # args= [0,0,0]
+        print("please use this script with 0 or 1 arguments!")
+        raise ValueError
 
-        # make befor Hlist
-        Hlist1 = []
-        for i in lines:
-            Hlist1.append(i.rstrip('\n'))
+    # make befor Hlist
+    Hlist1 = []
+    for i in lines:
+        Hlist1.append(i.rstrip('\n'))
 
-        # make after Hlist
-        Hlist2 = []
-        for k in range(len(lines)):
-            k = k + int(args[1])
-            Hname = 'H'+str(k).zfill(6)
-            Hlist2.append(Hname)
+    # make after Hlist
+    Hlist2 = []
+    for k in range(len(lines)):
+        k = k + firstH
+        Hname = 'H'+str(k).zfill(6)
+        Hlist2.append(Hname)
 
-        Hlist3 =[]
-        for n in range(len(lines2)):
-            for m in range(len(Hlist1)):
-                if re.search(Hlist1[m],lines2[n]):
-                    tmp = lines2[n].replace(Hlist1[m],Hlist2[m])
-                    Hlist3.append(tmp)
+    Hlist3 =[]
+    for n in range(len(lines2)):
+        for m in range(len(Hlist1)):
+            if re.search(Hlist1[m],lines2[n]):
+                tmp = lines2[n].replace(Hlist1[m],Hlist2[m])
+                Hlist3.append(tmp)
     
-                    # tmp = lines2[n].replace(Hlist1[m],Hlist2[m])
-                    # Hlist3.append()
+                # tmp = lines2[n].replace(Hlist1[m],Hlist2[m])
+                # Hlist3.append()
 
-        # output H conversion list 2022.4.27 K.S.
-        fileHConversionList = open("H_conversion_list.txt","w",newline="\n")
-        for l in range(len(Hlist1)):
-            fileHConversionList.write(Hlist1[l] + ' ' + Hlist2[l] + '\n')
-        fileHConversionList.close()
+    # output H conversion list 2022.4.27 K.S.
+    fileHConversionList = open("H_conversion_list.txt","w",newline="\n")
+    for l in range(len(Hlist1)):
+        fileHConversionList.write(Hlist1[l] + ' ' + Hlist2[l] + '\n')
+    fileHConversionList.close()
 
 
-        # kokokara Jan.22.2020
-        new_list3 = []
-        for l in range(len(lines2)): 
-            if re.match('\w',lines2[l]):
-                # print(lines[l])
-                new_list3.append(lines2[l])
-            elif re.match('~',lines2[l]):
-                # print(lines[l])
-                new_list3.append(lines2[l])
-            elif re.match('^     K',lines2[l]):
-                # print(lines[l])
-                new_list3.append(lines2[l])
-            elif re.match('^     J',lines2[l]):
-                # print(lines[l])
-                new_list3.append(lines2[l])
+    # kokokara Jan.22.2020
+    new_list3 = []
+    for l in range(len(lines2)): 
+        if re.match('\w',lines2[l]):
+            # print(lines[l])
+            new_list3.append(lines2[l])
+        elif re.match('~',lines2[l]):
+            # print(lines[l])
+            new_list3.append(lines2[l])
+        elif re.match('^     K',lines2[l]):
+            # print(lines[l])
+            new_list3.append(lines2[l])
+        elif re.match('^     J',lines2[l]):
+            # print(lines[l])
+            new_list3.append(lines2[l])
         
-        new_list4 = new_list3 + Hlist3  
-        with open(tmp3,'wt') as f:
-            f.writelines(new_list4)
+    new_list4 = new_list3 + Hlist3  
+    with open(tmp3,'wt') as f:
+        f.writelines(new_list4)
 
-        logFile.close()
+    logFile.close()
 
 except FileNotFoundError:
     print("Some previous files are not found in prempedit3.py!")
