cc5434a6e139de124ed27a1c3ba2d05646440595
COIAS-program <sugiuraks@elsi.jp>
2022-12-22 11:53:32 +0900

    webCOIASに向けて、書き出す・読み込むパラメータにidを追加した

--- COIASlibs/readparam.py
+++ COIASlibs/readparam.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3                                                                                                                  
 # -*- coding: UTF-8 -*
-# Timestamp: 2022/08/17 15:30 sugiura
+# Timestamp: 2022/12/22 12:00 sugiura
 ########################################################
 # param.txtを読み込み, パラメータを読み出し,
 # 辞書型に変換して返却する関数readparam()を定義したスクリプト.
@@ -13,7 +13,7 @@
 # また使用したパラメータをused_param.txtに書き出す関数
 # write_used_param(paramDict)も定義している.
 # 引数に辞書型で与えられたパラメータをused_param.txtに
-# 重複の内容に書き出す.
+# 重複の無いように書き出す.
 ########################################################
 import os
 
@@ -37,7 +37,7 @@ def readparam():
             if not is_num(content[1]):
                 raise ValueError("some second laws in param.txt are not numbers! content[1]={0}".format(content[1]))
 
-            if content[0]=="nd" or content[0]=="ar" or content[0]=="dm" or content[0]=="sn":
+            if content[0]=="nd" or content[0]=="ar" or content[0]=="dm" or content[0]=="sn" or content[0]=="id":
                 params[content[0]] = int(content[1])
             elif content[0]=="vt":
                 params[content[0]] = float(content[1])
@@ -57,7 +57,7 @@ def write_used_param(paramKey, paramValue):
         for line in lines:
             contents = line.split()
             key = contents[0]
-            if key=="nd" or key=="ar" or key=="dm" or key=="sn":
+            if key=="nd" or key=="ar" or key=="dm" or key=="sn" or key=="id":
                 value = int(contents[1])
             if key=="vt":
                 value = float(contents[1])
@@ -67,7 +67,7 @@ def write_used_param(paramKey, paramValue):
     existParamDict[paramKey] = paramValue
     f = open("used_param.txt","w")
     for k in existParamDict:
-        if k=="nd" or k=="ar" or k=="dm" or k=="sn":
+        if k=="nd" or k=="ar" or k=="dm" or k=="sn" or k=="id":
             f.write(k + " " + "{0:d} \n".format(existParamDict[k]))
         if k=="vt":
             f.write(k + " " + "{0:.1f} \n".format(existParamDict[k]))
