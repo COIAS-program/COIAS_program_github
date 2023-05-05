37dcd7e09469229af0580f888e161567f2982dd2
COIAS-program <sugiuraks@elsi.jp>
2023-05-05 22:10:58 +0900

    オリジナルCOIASではMySQLにアクセスしないので関係ないが、COIASlibs/COIAS_MySQL.pyをコピペしてウェブCOIASのものと同じ内容にした

--- COIASlibs/COIAS_MySQL.py
+++ COIASlibs/COIAS_MySQL.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*
-#Timestamp: 2023/01/27 20:30 sugiura
+# Timestamp: 2023/01/27 20:30 sugiura
 #####################################################################
 # MySQLのCOIASデータベースにconnectする関数とcloseする関数を記したスクリプト
 #####################################################################
 
 import pymysql
-from os.path import expanduser
+import sys
+
+sys.path.append("/home/COIASusers/web-coias-back-app/")
+from API import config
 
 ### function: connect to the database COIAS ###############
 def connect_to_COIAS_database():
-    ## get password
-    pwFileName = expanduser("~") + "/.pw/pwCOIASdb.txt"
-    f = open(pwFileName, "r")
-    pw = f.readline().rstrip("\n")
-    f.close()
-
     ## connect
-    connection = pymysql.connect(host="localhost",
-                                 user="dataHandler",
-                                 database="COIAS",
-                                 charset="utf8",
-                                 password=pw,
-                                 cursorclass=pymysql.cursors.DictCursor
-                                 )
+    connection = pymysql.connect(
+        host=config.DB_HOST,
+        port=int(config.DB_PORT),
+        user=config.DB_USER_NAME,
+        database=config.DB_DATABASE_NAME,
+        charset="utf8",
+        password=config.DB_PASSWORD,
+        cursorclass=pymysql.cursors.DictCursor,
+    )
     cursor = connection.cursor()
 
     return (connection, cursor)
+
+
 ###########################################################
 
 ### function: close the connection to the database COIAS ##
 def close_COIAS_database(connection, cursor):
     cursor.close()
     connection.close()
+
+
 ###########################################################
